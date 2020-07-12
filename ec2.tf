9e17d07bb819e548809ad45b53ec16d40ee91e0f
asdjkbfhjabf123 <49545318+asdjkbfhjabf123@users.noreply.github.com>
2020-07-12 17:12:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -20,24 +20,9 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"]
 }
-resource "aws_instance" "AWSServer1" {
-  ami           = "ami-0a54aef4ef3b5f881"
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
-  key_name        = "DockerLinux"
-  security_groups = ["launch-wizard-7"]
-  tags = {
-    Name = "Terraform Server1"
-  }
-}
-resource "aws_instance" "AWSServer2" {
-  ami           = "ami-0a54aef4ef3b5f881"
-  instance_type = "t2.micro"
-  key_name        = "DockerLinux"
-  security_groups = ["launch-wizard-7"]
-  tags = {
-    Name = "Terraform Server2"
-  }
-
 }
 output "ip"{
 value= "${aws_instance.web.public_ip}"
