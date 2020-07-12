e9975c08de0c1967f3441dc815fedb83eeba5005
asdjkbfhjabf123 <49545318+asdjkbfhjabf123@users.noreply.github.com>
2020-07-12 14:29:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,10 +23,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
-
-  tags {
-    Name = "HelloWorld"
-  }
 }
 output "ip"{
 value= "${aws_instance.web.public_ip}"
