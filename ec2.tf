0b8f85533c29668a90ce1d87c055e5c20aa057c4
asdjkbfhjabf123 <49545318+asdjkbfhjabf123@users.noreply.github.com>
2020-07-12 16:10:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -20,7 +20,11 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"]
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+}
+resource "aws_instance" "web2" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 }
