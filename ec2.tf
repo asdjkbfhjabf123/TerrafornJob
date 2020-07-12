08414b930351fc39b01bc7ee3039d1b59c394ea4
asdjkbfhjabf123 <49545318+asdjkbfhjabf123@users.noreply.github.com>
2020-07-12 15:52:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,11 +23,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_key_pair" "main" {
   public_key = "/home/ec2-user/DockerLinux.pem"
   }
-resource "aws_instance" "web1" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-}
-resource "aws_instance" "web2" {
+resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 }
